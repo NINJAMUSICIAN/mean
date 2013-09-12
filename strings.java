17f39770631eff309963c467f2ef5077cd07271e
Josh Sparks <j80mar4@gmail.com>
2013-09-12 08:44:12 -0600

    do you love me

---
+++ strings.java
@@ -0,0 +1,15 @@
+public class strings {
+
+	public static void main(String[] args) {
+		String str = new String("A string");
+		String s = "A string";
+
+		if (str.equals(s)) {
+			System.out.println("a");
+		}else{
+			System.out.println("b");
+		}
+
+	}
+
+}
